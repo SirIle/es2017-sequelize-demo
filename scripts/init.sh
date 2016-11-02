d37ea61756146c714c93e90cba2855359e93c304
Ilkka Anttonen <ilkka.anttonen@accenture.com>
2016-11-02 21:22:03 +0200

    Updated the client bundle

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo "Hello"
+wget https://get.docker.com/builds/Linux/x86_64/docker-1.11.0.tgz -O /tmp/docker-client.tgz
+tar xzf /tmp/docker-client.tgz
+sudo ln -s $PWD/docker/docker /usr/local/bin/docker
+cd files && unzip ucp-bundle.zip && eval $(<env.sh) && cd ..
+npm update -g npm
+cd ~/workspace
+npm install
+cd services/users && npm install
