b47ef4bd56f071eac90a59161788469a32b77fca
Ilkka Anttonen <ilkka.anttonen@accenture.com>
2016-11-03 01:12:32 +0200

    Added the installation of docker-compose

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,6 +2,8 @@
 wget https://get.docker.com/builds/Linux/x86_64/docker-1.11.0.tgz -O /tmp/docker-client.tgz
 tar xzf /tmp/docker-client.tgz
 sudo ln -s $PWD/docker/docker /usr/local/bin/docker
+sudo wget https://github.com/docker/compose/releases/download/1.9.0-rc2/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
 cd files && unzip ucp-bundle.zip && cd ..
 npm update -g npm
 npm install && cd services/users && npm install && cd ../..
